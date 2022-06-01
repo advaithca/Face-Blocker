cd0c5ea2f9ba7f53b1497da5c117f987e8cbde2c
advaithca <advaith29042002@gmail.com>
2022-06-01 05:51:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,18 +14,25 @@ image = st.file_uploader("Upload the image file",type=['jpg','jpeg'],accept_mult
 if image:
     fig = plt.figure()
     data =  plt.imread(image)
-    detector = MTCNN()
-    result_list = detector.detect_faces(data)
-    plt.axis('off')
-    plt.imshow(data)
-    ax = plt.gca()
-    for result in result_list:
-        x, y, width, height = result['box']
-        rect = Rectangle((x, y), width, height, fill=True, color='white')
-        ax.add_patch(rect)
-    st.pyplot(fig)
-    name = st.text_input("Name of output file: ")
-    if name:
+    with st.spinner("Detecting faces..."):
+        detector = MTCNN()
+        result_list = detector.detect_faces(data)
+        plt.axis('off')
+        plt.imshow(data)
+        ax = plt.gca()
+        for result in result_list:
+            x, y, width, height = result['box']
+            rect = Rectangle((x, y), width, height, fill=True, color='white')
+            ax.add_patch(rect)
+        st.pyplot(fig)
+    st.success("Detected faces and blocked the faces that were detected!")
+    
+    placeholder = st.empty()
+    with placeholder.container():
+        name = st.text_input("Name of output file: ")
+        clickme = st.button("Continue")
+    if name and clickme:
+        placeholder.empty()
         img = io.BytesIO()
         extent = ax.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
         plt.savefig(img, format='jpg',bbox_inches=extent)
